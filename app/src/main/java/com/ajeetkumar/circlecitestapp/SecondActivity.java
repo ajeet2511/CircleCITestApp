04b4322976c17d73d9cbc03f3a4c559b978f8b5a
Ajeet Kumar <ajeet.app.developer@gmail.com>
2015-09-22 16:43:45 +0530

    changed the compile sdks and support library version from 23 to 22

--- app/src/main/java/com/ajeetkumar/circlecitestapp/SecondActivity.java
+++ app/src/main/java/com/ajeetkumar/circlecitestapp/SecondActivity.java
@@ -11,6 +11,8 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
