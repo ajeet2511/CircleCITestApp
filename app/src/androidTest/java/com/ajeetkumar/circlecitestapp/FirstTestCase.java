4e6ae19c0821963239d32e8e9d9280a9a050197b
Ajeet Kumar <ajeet.app.developer@gmail.com>
2015-09-23 15:49:58 +0530

    added espresso and a simple test case for the app

---
+++ app/src/androidTest/java/com/ajeetkumar/circlecitestapp/FirstTestCase.java
@@ -0,0 +1,43 @@
+package com.ajeetkumar.circlecitestapp;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+/**
+ * Created by dev on 23/09/15.
+ */
+public class FirstTestCase extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mainActivity;
+
+    public FirstTestCase() {
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(getInstrumentation());
+        mainActivity = getActivity();
+    }
+
+    @Test
+    public void testChangeText_sameActivity() {
+        // Fill some dummy text into the text view
+        onView(withId(R.id.test_edit_text)).perform(typeText("just a test text."));
+
+        // Click Button to go the second activity
+        onView(withId(R.id.text_btn)).perform(click());
+
+    }
+}
--- app/src/main/java/com/ajeetkumar/circlecitestapp/MainActivity.java
+++ app/src/main/java/com/ajeetkumar/circlecitestapp/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //editText = (EditText) findViewById(R.id.test_edit_text);
     }
 
     @Override
