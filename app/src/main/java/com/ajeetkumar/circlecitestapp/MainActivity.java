619de156706ee4c326b3e54c0d3916de03529a85
Ajeet Kumar <ajeet.app.developer@gmail.com>
2015-10-20 11:23:13 +0530

    added disable keygaurd

--- app/src/main/java/com/ajeetkumar/circlecitestapp/MainActivity.java
+++ app/src/main/java/com/ajeetkumar/circlecitestapp/MainActivity.java
@@ -21,7 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
         //editText = (EditText) findViewById(R.id.test_edit_text);
 
-
+        try {
+            KeyguardManager mKeyGuardManager = (KeyguardManager) this.getSystemService(Context.KEYGUARD_SERVICE);
+            KeyguardManager.KeyguardLock mLock = mKeyGuardManager.newKeyguardLock("test");
+            mLock.disableKeyguard();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
